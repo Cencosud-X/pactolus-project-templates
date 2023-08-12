fb4ebc0d39cf900e59b8f6d175d256b938c3c87b
Johnny J. Huirilef Millán <johnnyjohn.huirilefmillan@externos-cl.cencosud.com>
2023-08-12 02:28:04 -0400

    feat(library): add type

---
+++ library/scripts/pre.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (runner, args) => {
+  try {
+    console.log('> PRE: Installing prerequisites (Library):');
+
+    const rc = args.rc;
+
+    // plubishable: a library marked as publishable (npm publish)
+    // buildable: a library marked as a "build" dist output
+    const isPublishable = !!rc.settings.publishable;
+    const isBuildable = !!rc.settings.buildable;
+    let publishableArg = '';
+    const buildableArg = `--buildable=${isBuildable}`;
+    const tagArg =  isBuildable || isPublishable ? '--tags=\"REQUIRED:GOLDEN\"' : '';
+    if (isPublishable) {
+      const npmOrganization = rc.settings.npm.organization;
+      const importPath = `@${npmOrganization}/${rc.path}`;
+      publishableArg = `--publishable --importPath=\"${importPath}\"`;
+    }
+
+    await runner.execute([
+      'npm install -D @nx/js',
+      `npx nx g @nx/js:lib ${rc.path} ${publishableArg} ${buildableArg} ${tagArg} --unitTestRunner=jest`
+    ], {
+      cwd: rc.workspace_path
+    })
+
+    console.log('> PRE: requisites ✅ DONE')
+
+  } catch(error) {
+    console.log(error);
+    throw new Error('failed to install library pre-requisites');
+  }
+}
