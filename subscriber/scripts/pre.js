b8698f3f7a49e9a8553e8c5df912685231fddfdf
Johnny J. Huirilef Millán <johnnyjohn.huirilefmillan@externos-cl.cencosud.com>
2023-08-12 03:05:33 -0400

    feat(subscriber): add type

---
+++ subscriber/scripts/pre.js
@@ -0,0 +1,31 @@
+module.exports = async (runner, args) => {
+  try {
+    console.log("> PRE: Installing prerequisites (Subscriber):");
+
+    const rc = args.rc;
+    
+    const topics = !!rc.settings.topics;
+    
+    const packages = [
+      "npm install --save-dev @nx/node:application",
+      "npm install --save @team_seki/subscriber-plugin@1.0.8",
+    ];
+    const installPubsub = Array.isArray(topics) && topics.some(topic => topic.provider === "PubSub") ? "npm install --save @team_seki/pubsub-streamer-plugin@1.1.4" : null;
+    const installKafka = Array.isArray(topics) && topics.some(topic => topic.provider === "Kafka") ? "npm install --save @team_seki/kafka-streamer-plugin@1.0.11" : null;
+    
+    await runner.execute(packages, { cwd: rc.workspace_path });
+    if (installPubsub !== null) {
+      console.log("> Installing prerequisites (Pubsub):");
+      await runner.execute(installPubsub, { cwd: rc.workspace_path });  
+    }
+    if (installKafka !== null) {
+      console.log("> Installing prerequisites (Kafka):");
+      await runner.execute(installKafka, { cwd: rc.workspace_path });  
+    }
+
+    console.log("> PRE: requisites ✅ DONE");
+  } catch (ex) {
+    console.error(ex);
+    throw new Error("Failed to install subscriber pre-requisites");
+  }
+};
