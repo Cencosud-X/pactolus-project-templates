3f5e38eb4a0483bb54769d38f431de3a1c3932f6
Johnny J. Huirilef Millán <johnnyjohn.huirilefmillan@externos-cl.cencosud.com>
2023-08-12 03:16:44 -0400

    fix(subscriber): add command necessary

--- subscriber/scripts/pre.js
+++ subscriber/scripts/pre.js
@@ -7,7 +7,8 @@ module.exports = async (runner, args) => {
     const topics = !!rc.settings.topics;
     
     const packages = [
-      "npm install --save-dev @nx/node:application --bundler=none --tags=\"REQUIRED:GOLDEN\" --unitTestRunner=jest --e2eTestRunner=none",
+      "npm install --save-dev @nx/node",
+      "npm install --save-dev webpack-merge",
       "npm install --save @team_seki/subscriber-plugin@1.0.8",
     ];
     const installPubsub = Array.isArray(topics) && topics.some(topic => topic.provider === "PubSub") ? "npm install --save @team_seki/pubsub-streamer-plugin@1.1.4" : null;
@@ -22,6 +23,8 @@ module.exports = async (runner, args) => {
       console.log("> Installing prerequisites (Kafka):");
       await runner.execute(installKafka, { cwd: rc.workspace_path });  
     }
+
+    await runner.execute(['npx nx g @nx/node:application --bundler=none --tags=\"REQUIRED:GOLDEN\" --unitTestRunner=jest --e2eTestRunner=none'], { cwd: rc.workspace_path })
 
     console.log("> PRE: requisites ✅ DONE");
   } catch (ex) {
