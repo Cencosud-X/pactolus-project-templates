f05e8d86a1b5a06ea8944f8d6c61799c97fb4c5f
Johnny J. Huirilef Millán <johnnyjohn.huirilefmillan@externos-cl.cencosud.com>
2023-08-15 23:07:22 -0400

    fix(subscriber): refactor logic install Kafka & Pubsub

--- subscriber/scripts/pre.js
+++ subscriber/scripts/pre.js
@@ -3,25 +3,23 @@ module.exports = async (runner, args) => {
     console.log("> PRE: Installing prerequisites (Subscriber):");
 
     const rc = args.rc;
-    
     const topics = !!rc.settings.topics;
-    
+
     const packages = [
       "npm install --save-dev @nx/node",
       "npm install --save-dev webpack-merge",
       "npm install --save @team_seki/subscriber-plugin@1.0.8",
     ];
-    const installPubsub = Array.isArray(topics) && topics.some(topic => topic.provider === "PubSub") ? "npm install --save @team_seki/pubsub-streamer-plugin@1.1.4" : null;
-    const installKafka = Array.isArray(topics) && topics.some(topic => topic.provider === "Kafka") ? "npm install --save @team_seki/kafka-streamer-plugin@1.0.11" : null;
-    
+    const installPubsub = Array.isArray(topics) && topics.some(topic => topic.provider === "PubSub");
+    const installKafka = Array.isArray(topics) && topics.some(topic => topic.provider === "Kafka");
     await runner.execute(packages, { cwd: rc.workspace_path });
     if (installPubsub !== null) {
-      console.log("> Installing prerequisites (Pubsub):");
-      await runner.execute(installPubsub, { cwd: rc.workspace_path });  
+      console.log("> 📦 Installing Pubsub:");
+      await runner.execute(['npm install --save @team_seki/pubsub-streamer-plugin@1.1.4'], { cwd: rc.workspace_path });  
     }
     if (installKafka !== null) {
-      console.log("> Installing prerequisites (Kafka):");
-      await runner.execute(installKafka, { cwd: rc.workspace_path });  
+      console.log("> 📦 Installing Kafka:");
+      await runner.execute(['npm install --save @team_seki/kafka-streamer-plugin@1.0.11'], { cwd: rc.workspace_path });  
     }
 
     await runner.execute([`npx nx g @nx/node:application ${rc.path} --framework=none  --bundler=webpack --tags=\"REQUIRED:GOLDEN\" --unitTestRunner=jest --e2eTestRunner=none`], { cwd: rc.workspace_path })
