96ce17cabbe99511526f3bca09394e99ce3e0b66
Johnny J. Huirilef Millán <johnnyjohn.huirilefmillan@externos-cl.cencosud.com>
2023-08-16 10:59:32 -0400

    fix(web): change config, add swc compiler

--- web/scripts/pre.js
+++ web/scripts/pre.js
@@ -5,6 +5,7 @@ module.exports = async (runner, args) => {
     const rc = args.rc;
     await runner.execute([
       'npm install -D @nx/web',
+      'npm install -D swc-loader',
       'npm install -D react-router-dom@6.15.0',
       'npm install -D webpack-merge',
       `npx nx g @nx/web:app ${rc.path} --unitTestRunner=jest --e2eTestRunner=none --bundler=webpack`
