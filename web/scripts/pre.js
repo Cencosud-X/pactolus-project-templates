15cf48a378e39b176d98f7024664edcfc525d434
Johnny J. Huirilef Millán <johnnyjohn.huirilefmillan@externos-cl.cencosud.com>
2023-08-15 23:36:10 -0400

    feat(web-react): add web-react template

---
+++ web/scripts/pre.js
@@ -0,0 +1,21 @@
+module.exports = async (runner, args) => {
+  try {
+    console.log('> PRE: Installing prerequisites (WEB):');
+
+    const rc = args.rc;
+    await runner.execute([
+      'npm install -D @nx/web',
+      'npm install -D react-router-dom@6.15.0',
+      'npm install -D webpack-merge',
+      `npx nx g @nx/web:app ${rc.path} --unitTestRunner=jest --e2eTestRunner=none --bundler=webpack`
+    ], {
+      cwd: rc.workspace_path
+    })
+
+    console.log('> PRE: requisites ✅ DONE')
+
+  } catch (ex) {
+    console.error(ex);
+    throw new Error('failed to install web pre-requisites');
+  }
+}
