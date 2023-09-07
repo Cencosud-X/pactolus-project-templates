5d360ac06aeb750a8847b96809c1253c374ff98b
Johnny J. Huirilef Millán <johnnyjohn.huirilefmillan@externos-cl.cencosud.com>
2023-09-07 10:21:08 -0300

    fix: refactor `astro/typerc.json` schema to include pipeline_kind as "web"

---
+++ docusaurus/scripts/post.js
@@ -0,0 +1,37 @@
+module.exports = async (runner, args) => {
+  try {
+    console.log("> POST: Cleansing:");
+
+    const rc = args.rc;
+    await runner.execute(
+      [
+        "rm -rf ./src/app",
+        "rm -rf ./src/test-setup.ts",
+        "rm -rf ./favicon.ico",
+        "rm -rf ./src/favicon.ico",
+        "rm -rf ./src/main.ts",
+        "rm -rf ./src/styles.css",
+        //'rm -rf ./src/assets',
+        "rm -rf ./src/environments",
+
+        "rm -rf ./.babelrc",
+        "rm -rf ./.eslintrc.json",
+        "rm -rf ./browserslist",
+        "rm -rf ./jest.config.ts",
+        "rm -rf ./tsconfig.spec.json",
+
+        "rm -rf ./src/index.html",
+        "rm -rf ./src/polyfills.ts",
+        "rm -rf ./src/browserslist",
+        "rm -rf ./src/assets",
+      ],
+      {
+        cwd: args.workspacePath,
+      }
+    );
+
+    console.log("> POST: cleansing process ✅ DONE");
+  } catch {
+    throw new Error("Failed to clean generators");
+  }
+};
---
+++ docusaurus/scripts/pre.js
@@ -0,0 +1,52 @@
+const path = require("path");
+
+module.exports = async (runner, args) => {
+  try {
+    console.log("> PRE: Installing prerequisites:");
+    const rc = args.rc;
+
+    let docusaurusVersion = "2.4.1";
+
+    // depend from the react version installed which version
+    // of docusaurus we need to install
+    try {
+      const packageJsonPath = path.join(rc.workspace_path, "package.json");
+      const packageDependencies = require(packageJsonPath).dependencies;
+      const reactVersion = packageDependencies["react"];
+      if (reactVersion.length > 0) {
+        const version = parseInt(reactVersion.split(".")[0]);
+        if (version >= 18) {
+          docusaurusVersion = "3.0.0-alpha.0";
+        }
+      }
+    } catch (ex) {
+      // maybe react wasnt installed at all
+      console.warn(
+        "Maybe react wasnt installed, so we take the default docusaurus version",
+        ex
+      );
+    }
+
+    await runner.execute(
+      [
+        "npm install -D @nrwl/web@14.4.3",
+
+        // Install docusaurus as the default
+        `npm install @docusaurus/core@${docusaurusVersion}`,
+        `npm install @docusaurus/preset-classic@${docusaurusVersion}`,
+        `npm install @docusaurus/theme-mermaid@${docusaurusVersion}`,
+        "npm install react-router-dom",
+
+        `npx nx g @nx/node:app --framework=none --e2eTestRunner=none  --projectNameAndRootFormat=derived ${rc.path}`,
+      ],
+      {
+        cwd: rc.workspace_path,
+      }
+    );
+
+    console.log("> PRE: requisites ✅ DONE");
+  } catch (ex) {
+    console.error(ex);
+    throw new Error("failed to install pre-requisites");
+  }
+};
